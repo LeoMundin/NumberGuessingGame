1cf6b29e89afea1214183ef79432ddf7f2cec384
leo <LM274971@falmouth.ac.uk>
2025-06-27 12:30:01 +0100

    Basic Project

--- NumberGuessingGame/NumberGuessingGame.cpp
+++ NumberGuessingGame/NumberGuessingGame.cpp
@@ -1,20 +1,54 @@
-// NumberGuessingGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <random>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    int numberOfAttempts = 5;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    int maxNumberRange;
+
+    cout << "Give me a whole number... ";
+    cin >> maxNumberRange;
+
+    int targetNumber = rand() % maxNumberRange + 1;
+
+    cout << "I'm thingking of a number between 0 - " << maxNumberRange << ", What is the number? \n";
+
+
+    for (int i = 0; i < numberOfAttempts; i++)
+    {
+
+        int currentGuess;
+            
+        cin >> currentGuess;
+
+        if (currentGuess == targetNumber) 
+        {
+
+            cout << "Well done! you guessed the number " << targetNumber << " with " << numberOfAttempts - i << " attempts left.";
+            return 1;
+
+        }
+        else if (currentGuess < targetNumber)
+        {
+
+            cout << "That guess is a little low, try again \n";
+
+        }
+        else if (currentGuess > targetNumber)
+        {
+
+            cout << "That guess is a little High, try again \n";
+
+        }
+
+    }
+
+    cout << "You ran out of attempts! the number was : " << targetNumber;
+    return 0;
+
+}    
